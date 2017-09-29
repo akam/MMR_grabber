fe226e9cb209d2312e43e7e3bcaa3cddfd78c8b5
akam <albertkam91@gmail.com>
2017-09-28 19:40:15 -0700

    added mmr estimate when solo mmr is unavaliable, sorted mmr from highest to lowest for avaliable solo

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 $(function() {
-  let steamID = [171320790, 44276560, 86985308, 78891550, 36480589, 293324544, 145715097, 60252548];
+  let steamID = [171320790, 44276560, 86985308, 78891550, 36480589, 293324544, 145715097, 60252548, 34903821, 174657509];
 
   ajaxRequestPromise().then((data)=>{
+    data.sort((a,b) => b.solo_competitive_rank - a.solo_competitive_rank);
     for(var i = 0; i < data.length; i++){
       if(data[i].profile){
-          let mmr = data[i].solo_competitive_rank;
+          let mmr = null;
+          if(data[i].solo_competitive_rank){
+            mmr = data[i].solo_competitive_rank;
+          } else {
+            mmr = data[i].mmr_estimate.estimate + '*';
+          }
           let IGN = data[i].profile.personaname;
           console.log(IGN);
           let newRow = $('<tr>');
