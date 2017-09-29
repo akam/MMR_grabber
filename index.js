4e38be4b8aeab7f415364e4d640bb2d0ad2dc7bc
akam <albertkam91@gmail.com>
2017-09-28 20:22:24 -0700

    changed strcuture to grab 20 most played with and rank them by mmr

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 $(function() {
-  let steamID = [171320790, 44276560, 86985308, 78891550, 36480589, 293324544, 145715097, 60252548, 34903821, 174657509];
-
-  ajaxRequestPromise().then((data)=>{
-    data.sort((a,b) => b.solo_competitive_rank - a.solo_competitive_rank);
+  var YOUR_STEAM_ID = prompt("Please enter your steam ID");
+  $.get(`https://api.opendota.com/api/players/${YOUR_STEAM_ID}/peers`).then((res) => {
+    var steamID = res.slice(0,20);
+    return steamID;
+  }).then((data) => {
+    return ajaxRequestPromise(data);
+  }).then((data)=>{
+    data.sort((a,b) => (b.solo_competitive_rank || b.mmr_estimate.estimate || 0) - (a.solo_competitive_rank || a.mmr_estimate.estimate || 0));
     for(var i = 0; i < data.length; i++){
       if(data[i].profile){
           let mmr = null;
           if(data[i].solo_competitive_rank){
             mmr = data[i].solo_competitive_rank;
-          } else {
+          } else if(data[i].mmr_estimate.estimate){
             mmr = data[i].mmr_estimate.estimate + '*';
+          } else {
+            mmr = 'N/A';
           }
           let IGN = data[i].profile.personaname;
           console.log(IGN);
@@ -21,11 +27,11 @@ $(function() {
           let table = $('table').append(newRow);
         }
     }
-  })
+  });
 
-  function ajaxRequestPromise(){
-    var arr = steamID.map(function(val){
-      return $.get(`https://api.opendota.com/api/players/${val}`);
+  function ajaxRequestPromise(ID){
+    var arr = ID.map(function(val){
+      return $.get(`https://api.opendota.com/api/players/${val.account_id}`);
     })
     return Promise.all(arr);
   }
